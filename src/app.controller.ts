b7be7b07e8281a54197ad481fd45c85df28f5bca
Said OUARRICH <warichsaid@gmail.com>
2023-11-22 21:11:03 +0100

    add controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Post, HttpCode, Header, Res, Redirect, Query, Param, Body } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { AppService } from './app.service';
+import CreateCatDto from "./dto/create-cat.dto";
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+@Controller('cats')
+export class CatsController {
+  // constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  @HttpCode(204)
+  create(@Body() body: CreateCatDto): string {
+    return `this action creates a cat: ${JSON.stringify(body)}`;
+  }
+
+  @Get(':id')
+  // @Redirect('https://nestjs.com', 301)
+  //@Header('cache-control', 'none')
+  getAll(@Query('e') e, @Param('id') id: string): {url: string} {
+    //es.header('Nappi', 'none').end("this action returns all cats");
+    if(e && e == 5 || id === "1") {
+      return { url : 'https://docs.nestjs.com/v5/'};
+    }
+    return {url : "this action returns all cats" };
   }
 }
