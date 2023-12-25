2c8dc13bdffea43f7fb9fac64c4b069e6293a005
Said OUARRICH <warichsaid@gmail.com>
2023-12-25 16:42:47 +0100

    remove cats module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,10 @@
-import {MiddlewareConsumer, Module, NestModule, RequestMethod} from '@nestjs/common';
-import { CatsModule } from './cats/cats.module';
-import { LoggerMiddlware } from './cats/logger.middlware';
-import { CatsController } from './cats/cats.controller';
+import {Module} from '@nestjs/common';
 import { ProfileModule } from './profile/profile.module';
 import { AuthModule } from './authentication/auth.module';
 
 
 @Module({
-    imports:[CatsModule, ProfileModule, AuthModule]
+    imports:[ProfileModule, AuthModule]
 })
-export class AppModule implements NestModule {
-    configure(consumer: MiddlewareConsumer) {
-        consumer
-            .apply(LoggerMiddlware)
-            .exclude('swagger')
-            .forRoutes(CatsController);
-
-    }
+export class AppModule {
 }
